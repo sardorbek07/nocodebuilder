bb2c7bb5f08f0b82d9045a0c5eefc12e296498bf
sardorbek07 <107134880+sardorbek07@users.noreply.github.com>
2025-12-14 16:32:56 +0500

    Update ewminiloyiha.js

--- miniloyiha.js
+++ miniloyiha.js
@@ -169,17 +169,15 @@ function saveSites(){
 }
 
 
-function formatDateTime(ts){
-  const d = new Date(ts || Date.now());
-
-  const day = String(d.getDate()).padStart(2,"0");
-  const month = String(d.getMonth()+1).padStart(2,"0");
-  const year = d.getFullYear();
-  const hour = String(d.getHours()).padStart(2,"0");
-  const minute = String(d.getMinutes()).padStart(2,"0");
-
-  return day+"-"+month+"-"+year+"; Soat: "+hour+":"+minute+" da";
+function formatDate(ts){
+  const d=new Date(ts||Date.now());
+  return d.toLocaleDateString("uz-UZ");
 }
+function formatDateTime(ts){
+  const d=new Date(ts||Date.now());
+  return d.toLocaleDateString("uz-UZ")+" "+d.toLocaleTimeString("uz-UZ",{hour:"2-digit",minute:"2-digit"});
+}
+
 
 
 
