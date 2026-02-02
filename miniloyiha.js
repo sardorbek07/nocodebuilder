61d634bfe3a9c58ebb6096606c9633720c7b693f
sardorbek07 <107134880+sardorbek07@users.noreply.github.com>
2026-02-02 17:37:22 +0500

    Update miniloyiha.js

--- miniloyiha.js
+++ miniloyiha.js
@@ -2694,19 +2694,16 @@ ov.style.padding = "0";
 
       tEl.parentElement.style.padding = "70px 18px 10px 18px";
       body.style.padding = "10px 18px 22px 18px";
-    }else{
-      ov.style.position = "fixed";
-ov.style.inset = "0";
-  ov.style.display = "flex";
-  ov.style.alignItems = "center";
-  ov.style.justifyContent = "center";
-  ov.style.padding = "20px";
+   }else{
+  ov.style.display = "block";
+  ov.style.padding = "0";
 
   modal.style.margin = "0";
-      modal.style.position = "relative";
-modal.style.left = "0";
-modal.style.top = "0";
-modal.style.transform = "none";
+  modal.style.position = "fixed";
+  modal.style.left = "50%";
+  modal.style.top = "50%";
+  modal.style.transform = "translate(-50%,-50%)";
+
   modal.style.width = "calc(100% - 40px)";
   modal.style.maxWidth = "520px";
   modal.style.minHeight = "";
